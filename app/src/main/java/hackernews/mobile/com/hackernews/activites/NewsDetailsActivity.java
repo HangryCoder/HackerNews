883a0f13f29f4a7b3a6d07cdecaf7d8dfa7852b6
Sonia Wadji <soniawadji@gmail.com>
2018-03-11 14:20:55 +0530

    Commit "1.1" Working on NewsDetailScreen.. Made a structure of Collapsing Toolbar with Tabs.. Still requires adjustments..

--- app/src/main/java/hackernews/mobile/com/hackernews/activites/NewsDetailsActivity.java
+++ app/src/main/java/hackernews/mobile/com/hackernews/activites/NewsDetailsActivity.java
@@ -2,12 +2,18 @@ package hackernews.mobile.com.hackernews.activites;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import hackernews.mobile.com.hackernews.R;
+import hackernews.mobile.com.hackernews.adapter.ViewPagerAdapter;
+import hackernews.mobile.com.hackernews.fragments.ArticleFragment;
+import hackernews.mobile.com.hackernews.fragments.CommentFragment;
 
 /**
  * Created by soniawadji on 11/03/18.
@@ -17,6 +23,12 @@ public class NewsDetailsActivity extends AppCompatActivity {
 
     @BindView(R.id.toolBar)
     Toolbar toolbar;
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+    @BindView(R.id.collapsibleToolbarLayout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,5 +38,18 @@ public class NewsDetailsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent)); // transperent color = #00000000
+
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CommentFragment(), getResources().getString(R.string.comments));
+        adapter.addFragment(new ArticleFragment(), getResources().getString(R.string.article));
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/hackernews/mobile/com/hackernews/fragments/CommentFragment.java
@@ -0,0 +1,24 @@
+package hackernews.mobile.com.hackernews.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hackernews.mobile.com.hackernews.R;
+
+/**
+ * Created by soniawadji on 11/03/18.
+ */
+
+public class CommentFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_comment, container, false);
+        return view;
+    }
+}
