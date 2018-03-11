8bbac651b8fbcca729d6527ff4b369e9428ddd58
Sonia Wadji <soniawadji@gmail.com>
2018-03-11 14:36:44 +0530

    Commit "1.2" Working on NewsDetailScreen.. Made a structure of Collapsing Toolbar with Tabs.. Still requires adjustments.. Added WebView for Articles Screen..DONE :)

--- app/src/main/java/hackernews/mobile/com/hackernews/fragments/ArticleFragment.java
+++ app/src/main/java/hackernews/mobile/com/hackernews/fragments/ArticleFragment.java
@@ -6,7 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import hackernews.mobile.com.hackernews.R;
 
 /**
@@ -15,10 +19,27 @@ import hackernews.mobile.com.hackernews.R;
 
 public class ArticleFragment extends Fragment {
 
+    @BindView(R.id.webView)
+    WebView webView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_article, container, false);
+
+        ButterKnife.bind(this, view);
+
+        webView.setWebViewClient(new ArticleBrowser());
+        webView.loadUrl("http://www.google.com");
+
         return view;
+    }
+
+    private class ArticleBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
